ac1207b5d55f6a77d345038d5f91405556ae8d8a
Ching Han Huang <60158050+chinghan95@users.noreply.github.com>
2020-03-24 21:33:15 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# GoodTimes March 24, 2020
+Demo program to show a simple way to measure the time spend for a given operation.
+
+Introduce a simple *lambda expression* for printing a **List**, and for changing all of the values in that **List**.
---
+++ src/GoodTimes.java
@@ -0,0 +1,15 @@
+
+public class GoodTimes {
+	    
+    public static void main(String[] args) {
+	
+	   	BetterTimes betterTimes = new BetterTimes();
+	
+	   	betterTimes.firstAttemtToMeasureTimeDifferende();
+	
+	   	betterTimes.createRandomwithMath();
+		
+	   	betterTimes.createRandomWithRandom();
+    }
+
+}
